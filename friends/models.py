f6e08f1707aebfb170c5bf1e0967c974dd517e74
brahim024 <ifninos168@gmail.com>
2021-01-16 13:32:47 +0100

    add friends models

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Friend(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+class Belonging(models.Model):
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+class Borrowed(models.Model):
+	what = models.ForeignKey(Belonging,on_delete=models.CASCADE)
+	to = models.ForeignKey(Friend,on_delete=models.CASCADE)
+	when = models.DateTimeField(auto_now_add=True)
+	returned = models.DateTimeField(null=True,blank=True)
+
+	def __str__(self):
+		return self.what
